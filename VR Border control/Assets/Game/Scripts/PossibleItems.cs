7a8076f78c9754c72952cca306798c544670450e
Unknown <davidjejans@hotmail.nl>
2018-10-27 22:34:45 +0200

    Reimport again because git was being mean

---
+++ VR Border control/Assets/Game/Scripts/PossibleItems.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossibleItems : MonoBehaviour {
+	public List<GameObject> m_PossibleItems;
+	public List<int> m_NotAllowedThrough;
+	public List<int> m_AllowedThrough;
+}
